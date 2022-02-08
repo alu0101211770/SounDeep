da68a65d6ec4dc3e83086b110d0693aaadb0d3fd
alu0101211770 <alu0101211770@ull.edu.es>
2022-02-08 21:05:47 +0000

    Remove uneeded files

--- src/classify.py
+++ src/classify.py
@@ -1,5 +1,5 @@
-from operator import ge
-from pydub import AudioSegment
+# from operator import ge
+# from pydub import AudioSegment
 from clip import generateFeaturedClip
 from split_wav import splitIntoFragments
 from model import load_model
@@ -11,7 +11,6 @@ import numpy as np
 def classify_theme(theme_file):
     model = load_model('./model/model_architecture.json',
                       './model/model_weights.h5')
-    #theme = AudioSegment.from_wav(theme_file)
     clip = generateFeaturedClip(theme_file)
     splitIntoFragments(theme_file, clip, './tmp/')
 
@@ -35,7 +34,6 @@ def classify_theme(theme_file):
     genres = genres.split()
     classified_genres = []
     for i in range(3):
-      # current_max_value = max(result)
       current_max_index = np.argmax(result)
       classified_genres.append(genres[current_max_index])
       del(result[current_max_index])
